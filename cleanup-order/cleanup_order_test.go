a1a9540ca3e9e2c0167f706ca3d80e0a15ef6978
Xuanyu Wang <xuanyu.wang@outlook.com>
2024-02-11 20:48:02 -0500

    Update log of cleanup order

--- cleanup-order/cleanup_order_test.go
+++ cleanup-order/cleanup_order_test.go
@@ -7,6 +7,8 @@ import (
 	"github.com/stretchr/testify/suite"
 )
 
+var tearDownSubTest []string
+
 type TestCleanupOrder struct {
 	suite.Suite
 }
@@ -14,84 +16,94 @@ type TestCleanupOrder struct {
 func (s *TestCleanupOrder) SetupSuite() {
 	fmt.Println("Run Interface SetupAllSuite: SetupSuite")
 	s.T().Cleanup(func() {
-		fmt.Println("Registered Interface SetupAllSuite: SetupSuite")
+		fmt.Println("Cleanup Interface SetupAllSuite: SetupSuite")
 	})
 }
 
 func (s *TestCleanupOrder) SetupTest() {
 	fmt.Println("Run Interface SetupTestSuite: SetupTest")
 	s.T().Cleanup(func() {
-		fmt.Println("Registered Interface SetupTestSuite: SetupTest")
+		fmt.Println("Cleanup Interface SetupTestSuite: SetupTest")
 	})
 }
 
 func (s *TestCleanupOrder) TeatDownSuite() {
 	fmt.Println("Run Interface TearDownAllSuite: TearDownSuite")
 	s.T().Cleanup(func() {
-		fmt.Println("Registered Interface TearDownAllSuite: TearDownSuite")
+		fmt.Println("Cleanup Interface TearDownAllSuite: TearDownSuite")
 	})
 }
 
 func (s *TestCleanupOrder) TearDownTest() {
 	fmt.Println("Run Interface TearDownTestSuite: TearDownTest")
 	s.T().Cleanup(func() {
-		fmt.Println("Registered Interface TearDownTestSuite: TearDownTest")
+		fmt.Println("Cleanup Interface TearDownTestSuite: TearDownTest")
 	})
 }
 
 func (s *TestCleanupOrder) BeforeTest(suiteName, testName string) {
-	fmt.Println("Run Interface BeforeTest: BeforeTest")
+	fmt.Printf("Run Interface BeforeTest: BeforeTest for suite %s - test %s\n", suiteName, testName)
 	s.T().Cleanup(func() {
-		fmt.Println("Registered Interface BeforeTest: BeforeTest")
+		fmt.Printf("Cleanup Interface BeforeTest: BeforeTest for suite %s - test %s\n", suiteName, testName)
 	})
 }
 
 func (s *TestCleanupOrder) AfterTest(suiteName, testName string) {
-	fmt.Println("Run Interface AfterTest: AfterTest")
+	fmt.Printf("Run Interface AfterTest: AfterTest for suite %s - test %s\n", suiteName, testName)
 	s.T().Cleanup(func() {
-		fmt.Println("Registered Interface AfterTest: AfterTest")
+		fmt.Printf("Cleanup Interface AfterTest: AfterTest for suite %s - test %s\n", suiteName, testName)
 	})
 }
 
 func (s *TestCleanupOrder) HandleStats(suiteName string, stats *suite.SuiteInformation) {
-	fmt.Println("Run Interface WithStats: HandleStats")
+	fmt.Printf("Run Interface WithStats: HandleStats for suite %s\n", suiteName)
 	s.T().Cleanup(func() {
-		fmt.Println("Registered Interface WithStats: HandleStats")
+		fmt.Printf("Cleanup Interface WithStats: HandleStats for suite %s\n", suiteName)
 	})
 }
 
 func (s *TestCleanupOrder) SetupSubTest() {
 	fmt.Println("Run Interface SetupSubTest: SetupSubTest")
 	s.T().Cleanup(func() {
-		fmt.Println("Registered Interface SetupSubTest: SetupSubTest")
+		fmt.Println("Cleanup Interface SetupSubTest: SetupSubTest")
 	})
 }
 
 func (s *TestCleanupOrder) TearDownSubTest() {
 	fmt.Println("Run Interface TearDownSubTest: TearDownSubTest")
 	s.T().Cleanup(func() {
-		fmt.Println("Registered Interface TearDownSubTest: TearDownSubTest")
+		fmt.Println("Cleanup Interface TearDownSubTest: TearDownSubTest")
+		fmt.Printf("\tTear down sub test %v\n", tearDownSubTest)
 	})
 }
 
 // Test case
 func (s *TestCleanupOrder) TestCaseA() {
-	fmt.Println("Run TestCaseA - Top level")
+	fmt.Println("Run TestCase A")
 	s.T().Cleanup(func() {
-		fmt.Println("Registered at TestCaseA - Top level")
+		fmt.Println("Cleanup TestCase A")
 	})
 
-	s.Run("TestCaseA - Subtest A", func() {
+	s.Run("Sub-TestCase A", func() {
+		fmt.Println("Run Sub-TestCase A")
 		s.T().Cleanup(func() {
-			fmt.Println("Registered at SubTestCaseA")
+			fmt.Println("Cleanup Sub-TestCase A")
 		})
-		fmt.Println("Run Sub-TestCaseA")
-		s.Run("TestCaseA - Sub-subtest A", func() {
-			s.T().Cleanup(func() {
-				fmt.Println("Registered at Sub-sub-TestCaseA")
-			})
+		tearDownSubTest = append(tearDownSubTest, "Sub-TestCase A")
 
-			fmt.Println("Run Sub-sub-TestCaseA")
+		s.Run("Sub-Sub-TestCase A", func() {
+			s.T().Cleanup(func() {
+				fmt.Println("Cleanup Sub-Sub-TestCase A")
+			})
+			fmt.Println("Run Sub-Sub-TestCase A")
+			tearDownSubTest = append(tearDownSubTest, "Sub-Sub-TestCase A")
+		})
+		s.Run("Sub-Sub-TestCase B", func() {
+			s.T().Cleanup(func() {
+				fmt.Println("Cleanup Sub-Sub-TestCase B")
+			})
+			fmt.Println("Run Sub-Sub-TestCase B")
+			tearDownSubTest = append(tearDownSubTest, "Sub-Sub-TestCase B")
 		})
 	})
 }
