3bd799791b7d9d18432846c732315ba5660282aa
Xuanyu Wang <xuanyu.wang@outlook.com>
2024-12-26 18:56:43 -0500

    Add example of mock leak

--- go.mod
+++ go.mod
@@ -1,13 +1,17 @@
 module github.com/xuanyuwang/testify-examples
 
-go 1.21.7
+go 1.22
 
-require github.com/stretchr/testify v1.8.4
+toolchain go1.23.3
+
+require (
+	github.com/golang/mock v1.6.0
+	github.com/stretchr/testify v1.9.0
+)
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/golang/mock v1.6.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/stretchr/objx v0.5.0 // indirect
+	github.com/stretchr/objx v0.5.2 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- mock-and-subtest/mock_and_subtest_test.go
+++ mock-and-subtest/mock_and_subtest_test.go
@@ -1,1 +1,54 @@
 package mockandsubtest
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+)
+
+// Prod code
+type ExternalService interface {
+	Work()
+}
+
+type Server struct {
+	externalService ExternalService
+}
+
+func NewServer(externalService ExternalService) *Server {
+	return &Server{
+		externalService: externalService,
+	}
+}
+
+// Test code
+type ServerSuite struct {
+	suite.Suite
+	ExternalService *MockExternalService
+	Server
+}
+
+func TestServerSuite(t *testing.T) {
+	suite.Run(t, &ServerSuite{})
+}
+
+// Run before all test cases
+func (s *ServerSuite) SetupSuite() {
+	s.ExternalService = NewMockExternalService(s.T())
+	s.Server = Server{externalService: s.ExternalService}
+}
+
+func (s *ServerSuite) TearDownTest() {
+	// s.ExternalService.AssertExpectations(s.T())
+}
+
+func (s *ServerSuite) TestA() {
+	fmt.Println("TestA is running")
+	s.ExternalService.EXPECT().Work().Times(1)
+}
+
+func (s *ServerSuite) TestB() {
+	fmt.Println("TestB is running")
+	s.Server.externalService.Work()
+}
